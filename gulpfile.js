b8fa84b910f198ef4ba37fa585f06c527d134005
nitayneeman <nitayneeman@users.noreply.github.com>
2018-01-11 22:41:28 +0200

    Added gulp

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var rename = require('gulp-rename');
+
+gulp.task('concat', function() {
+  return gulp.src('./build/*.js')
+    .pipe(concat('made-with-love.js'))
+    .pipe(gulp.dest('./dist/'));
+});
+
+gulp.task('rename', function() {
+  return gulp.src('./build/styles.bundle.css')
+    .pipe(rename('made-with-love.css'))
+    .pipe(gulp.dest('./dist'));
+});
