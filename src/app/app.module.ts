a0a98cf4679b908822f14e65e552beec709ac50b
nitayneeman <nitayneeman@users.noreply.github.com>
2018-01-11 20:55:17 +0200

    Reorganized app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
 import { MadeWithLoveComponent } from './made-with-love/made-with-love.component';
 
+export const customElements = [
+  MadeWithLoveComponent
+];
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    MadeWithLoveComponent
-  ],
   imports: [
     BrowserModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [
+    ...customElements,
+  ],
+  entryComponents: [
+    ...customElements
+  ]
 })
-export class AppModule { }
+export class AppModule {
+  ngDoBootstrap() { }
+}
