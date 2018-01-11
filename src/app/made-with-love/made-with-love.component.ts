b1fc0ed10e9d18d2d1a9b945dd25beea1d738a12
nitayneeman <nitayneeman@users.noreply.github.com>
2018-01-11 20:53:23 +0200

    Added template and logic

--- src/app/made-with-love/made-with-love.component.ts
+++ src/app/made-with-love/made-with-love.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'made-with-love',
@@ -6,10 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./made-with-love.component.scss']
 })
 export class MadeWithLoveComponent implements OnInit {
+  @Input()
+  public name: string;
 
-  constructor() { }
+  @Input()
+  public url: string;
+
+  @Input()
+  public color: string = 'red';
+
+  @Input()
+  public size: number = 1;
 
   ngOnInit() {
+    if(!this.name || this.name.length === 0) {
+      console.error(`Name attribute must be provided!`);
+    }
   }
-
 }
