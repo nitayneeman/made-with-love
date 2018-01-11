89a77b07004030a00fdb76fca88e32ad570ae0c9
nitayneeman <nitayneeman@users.noreply.github.com>
2018-01-11 20:51:48 +0200

    Created an empty component for made-with-love

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
+import { MadeWithLoveComponent } from './made-with-love/made-with-love.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MadeWithLoveComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/made-with-love/made-with-love.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-made-with-love',
+  templateUrl: './made-with-love.component.html',
+  styleUrls: ['./made-with-love.component.scss']
+})
+export class MadeWithLoveComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
