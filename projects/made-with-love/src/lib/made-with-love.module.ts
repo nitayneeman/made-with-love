6359a976bc6530f0e7c9641fa3aab9ffe98f9c26
nitayneeman <nitayneeman@users.noreply.github.com>
2018-07-12 22:40:21 +0300

    Created made-with-love module

--- projects/made-with-love/src/lib/made-with-love.module.ts
+++ projects/made-with-love/src/lib/made-with-love.module.ts
@@ -1,9 +1,20 @@
-import { NgModule } from '@angular/core';
-import { MadeWithLoveComponent } from './made-with-love.component';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA, Injector } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { createCustomElement } from '@angular/elements';
+
+import { MadeWithLoveComponent } from './made-with-love/made-with-love.component';
 
 @NgModule({
-  imports: [],
+  imports: [CommonModule],
   declarations: [MadeWithLoveComponent],
+  entryComponents: [MadeWithLoveComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   exports: [MadeWithLoveComponent]
 })
-export class MadeWithLoveModule {}
+export class MadeWithLoveModule {
+  constructor(private injector: Injector) {
+    const madeWithLoveElement = createCustomElement(MadeWithLoveComponent, { injector });
+
+    customElements.define('made-with-love', madeWithLoveElement);
+  }
+}
--- projects/made-with-love/src/public_api.ts
+++ projects/made-with-love/src/public_api.ts
@@ -2,6 +2,5 @@
  * Public API Surface of made-with-love
  */
 
-export * from './lib/made-with-love.service';
-export * from './lib/made-with-love.component';
+export * from './lib/made-with-love/made-with-love.component';
 export * from './lib/made-with-love.module';
