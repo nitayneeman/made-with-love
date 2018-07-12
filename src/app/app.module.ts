b7708db70158cdeb30596c3b246d075de3617c6b
nitayneeman <nitayneeman@users.noreply.github.com>
2018-07-12 22:45:15 +0300

    Created app component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,13 @@
-import { NgModule, Injector } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { createCustomElement } from '@angular/elements';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+import { MadeWithLoveModule } from 'angular-made-with-love';
 
-import { MadeWithLoveComponent } from './made-with-love/made-with-love.component';
+import { AppComponent } from './app.component';
 
 @NgModule({
-  imports: [BrowserModule],
-  declarations: [MadeWithLoveComponent],
-  entryComponents: [MadeWithLoveComponent]
+  imports: [BrowserModule, MadeWithLoveModule],
+  declarations: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  bootstrap: [AppComponent]
 })
-export class AppModule {
-  constructor(private injector: Injector) {
-    const customElement = createCustomElement(MadeWithLoveComponent, { injector });
-    customElements.define('made-with-love', customElement);
-  }
-
-  ngDoBootstrap() {}
-}
+export class AppModule {}
